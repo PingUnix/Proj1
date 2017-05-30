6b2c9cfb59c4fa4a246c41e9bc694f9c1af31de9
SandyLinux <mailliuping@gmail.com>
2017-05-30 17:46:44 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component,Directive } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<p>Hello <span [class.is-awesome] = "inputElement.value==='yes'" >helloo {{name}}</span></p>
+    <label>input name</label>
+    <input type="text" name = "name" #inputElement  (keyup)="0"/>
+  
+    `,
+    styleUrls:['./../test.css']
+
+})
+export class AppComponent  { 
+  name = 'Angular'; }
