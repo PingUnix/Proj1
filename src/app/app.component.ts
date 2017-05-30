6258326aebe249e3e95742384b3e95fd7932f47e
SandyLinux <mailliuping@gmail.com>
2017-05-30 17:36:15 -0400

    modify color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component,Directive } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<p>Hello <span [class.is-awesome] = "inputElement.value==='yes'" >helloo {{name}}</span></p>
+  template: `<p>Hello <span [class.is-awesome] = "inputElement.value==='YES'" >helloo {{name}}</span></p>
     <label>input name</label>
     <input type="text" name = "name" #inputElement  (keyup)="0"/>
   
