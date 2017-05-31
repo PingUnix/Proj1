d71ad602623ce1efbcd1c64b34b1270bac3a380a
SandyLinux <mailliuping@gmail.com>
2017-05-31 17:50:19 -0400

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component,Directive } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `<p>Hello <span [class.is-awesome] = "inputElement.value==='yes'" >helloo {{name}}</span></p>
+    <br/><br/>
+    <span [style.color]="'#CD5'">the title is {{title}}</span>
     <label>input name</label>
+
     <input type="text" name = "name" #inputElement  (keyup)="0"/>
     <button [disabled]="inputElement.value !== 'yes'" > only enabled when input 'yes'</button>
     `,
@@ -11,4 +14,6 @@ import { Component,Directive } from '@angular/core';
 
 })
 export class AppComponent  { 
-  name = 'Angular'; }
+  name = 'Angular'; 
+  title = " quick start";
+}
