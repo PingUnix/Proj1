b2c0854f9c398efb9d830c89681bbcba44757391
SandyLinux <mailliuping@gmail.com>
2017-06-01 14:57:19 -0400

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component,Directive } from '@angular/core';
 import { OnInit} from '@angular/core';
+//import { ngModel } from '@angular/core';
+
 @Component({
   selector: 'my-app',
-  template: `<p>Hello {{name}} <span [class.is-awesome] = "inputElement.value==='yes'" >helloo {{name}}</span></p>
+  template: `
+    <a href="" > a link </a>
+    <input [(ngModel)]="name">
+     <input [style.color]="colors" text="the title is"> 
+    <h1 (click)="onSelect()" [class.clicked]="showDetail === true"> A title here</h1>
+    <p>Hello {{name}} <span [class.is-awesome] = "inputElement.value==='yes'" >helloo {{name}}</span></p>
     <br/><br/>
     <div *ngIf="name === 'MAX'">
     <span [style.color]="colors">the title is {{title}}</span>
@@ -14,6 +21,7 @@ import { OnInit} from '@angular/core';
     <test >this is the test component</test>
     <button (click)="fun()"> click me </button>
     <input type="text" name = "input" [value] = "name" /> {{name}}
+    
     `,
     styleUrls:['./../test.css']
 
@@ -38,5 +46,8 @@ export class AppComponent implements OnInit {
     //return null;
     this.name = "MAX";
     this.title = this.name + "quick start";
-}
+  }
+  onSelect(){
+    this.showDetail = !this.showDetail;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { TestComponent } from './test.component';
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent,TestComponent ],
+  imports:      [ BrowserModule,FormsModule ],
+  declarations: [ AppComponent,TestComponent],
   bootstrap:    [ AppComponent ],
 
 })
